a6068681e817c98a8a3e5fc8d1011fff4f2593cd
devinyan <devinyan@tencent.com>
2024-11-25 21:15:08 +0800

    增加获取对应的dentry结构，并且打印对应的目录名称

--- bpf-apps/mkdir.bpf.c
+++ bpf-apps/mkdir.bpf.c
@@ -2,17 +2,39 @@
 
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
 
 
 SEC("kprobe/vfs_mkdir")
-int kprobe_vfs_mkdir(void *ctx)
+int kprobe_vfs_mkdir(struct pt_regs *ctx)
 {
-    bpf_printk("kprobe,mkdir (vfs hook point)\n");
+    struct dentry copyDentry;
+    struct dentry* dentry;
+    dentry = (struct dentry*)PT_REGS_PARM3(ctx);
+
+    if (!dentry)
+    {
+        return 0;
+    }
+
+    bpf_probe_read(&copyDentry, sizeof(copyDentry), dentry);
+
+    bpf_printk("kprobe ,mkdir (vfs hook point) %s---%s %d\n",copyDentry.d_iname,copyDentry.d_name.name,copyDentry.d_name.len);
+
+    struct dentry copyDentryParent;
+    if (!(copyDentry.d_parent))
+    {
+        return 0;
+    }
+
+    bpf_probe_read(&copyDentryParent, sizeof(copyDentryParent), copyDentry.d_parent);
+
+    bpf_printk("kprobe parent,mkdir (vfs hook point) %s---%s %d\n",copyDentryParent.d_iname,copyDentryParent.d_name.name,copyDentryParent.d_name.len);
     return 0;
 };
 
 SEC("kretprobe/vfs_mkdir")
-int kretpobe_mkdir(void *ctx)
+int kretpobe_mkdir(struct pt_regs *ctx)
 {
     bpf_printk("kretprobe,mkdir (vfs hook point)\n");
     return 0;
--- bpf-apps/mkdir.h
+++ bpf-apps/mkdir.h
@@ -5,5 +5,12 @@
 #ifndef MKDIR_H
 #define MKDIR_H
 
+struct trace {
+    unsigned int tgid;
+    unsigned int action;  // system call number
+    unsigned long ts;
+    long ret;
+    int ready;  // // 0: empty or writing; 1: readable; 2: useless
+};
 
 #endif //MKDIR_H
